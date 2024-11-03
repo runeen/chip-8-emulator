756b57f008a8843be0dbb2e858cbd8ee69ff8701
Botez <a.eugen23@yahoo.com>
2024-11-03 19:43:54 +0200

    toti incepem undeva, iar eu incep aici

---
+++ chip-8/Instruction.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chip_8
+{
+    class Instruction
+    {
+        byte tip;
+        byte x;
+        byte y;
+        byte n;
+        byte nn;
+        ushort nnn;
+
+        public Instruction(ushort input)
+        {
+
+            //TODO: transforma instructiuni in bucati tip, x, y, n, nn, nnn
+            BitArray instructiune = new BitArray(16);
+
+
+
+
+            byte pow = 1;
+            for(int i = 0; i < 8; i++)
+            {
+                Console.WriteLine("instructiune[{0}] = {1}", i, instructiune[i]);
+                if (instructiune[i]) tip += pow;
+                pow *= 2;
+            }
+
+            pow = 1;
+            for (int i = 8; i < 16; i++)
+            {
+                if (instructiune[i]) x += pow;
+                pow *= 2;
+            }
+
+            pow = 1;
+            for (int i = 16; i < 24; i++)
+            {
+                if (instructiune[i]) y += pow;
+                pow *= 2;
+            }
+
+            pow = 1;
+            for (int i = 24; i < 32; i++)
+            {
+                if (instructiune[i]) n += pow;
+                pow *= 2;
+            }
+
+
+            Console.WriteLine("Avem: {0}, {1}, {2}, {3}", tip, x, y, n);
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ chip-8/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chip_8
+{
+    class Program
+    {
+        ushort PC;
+        ushort I;
+        byte delayTimer;
+        byte soundTimer;
+        Stack<ushort> stack;
+        bool[] display;
+        ushort[] memory;
+        byte[] registers;
+
+        static void Main(string[] args)
+        {
+            Instruction test = new Instruction(12345);
+        }
+
+
+        private void Emulator()
+        {
+            registers = new byte[16];
+            memory = new ushort[4096];
+            display = new bool[2048];
+            stack = new Stack<ushort>();
+
+            while (true)
+            {
+                Decode(Fetch());
+                //Execute();
+            }
+        }
+
+        private Instruction Fetch()
+        {
+            // Read instruction
+
+            PC += 2;
+            return new Instruction(1234);
+        }
+
+        private void Decode(Instruction instruction)
+        {
+            switch (instruction)
+            {
+                default:
+                    break;
+            }
+        }
+
+    }
+}
