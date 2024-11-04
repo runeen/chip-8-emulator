76e13babc26528d893985bd265bf5696269d49d3
Botez <a.eugen23@yahoo.com>
2024-11-04 08:47:47 +0200

    am implementat jumatate din instructiunile din programu basic ibm

--- chip-8/Instruction.cs
+++ chip-8/Instruction.cs
@@ -9,54 +9,83 @@ namespace chip_8
 {
     class Instruction
     {
-        byte tip;
-        byte x;
-        byte y;
-        byte n;
-        byte nn;
-        ushort nnn;
+        public byte tip;
+        public byte x;
+        public byte y;
+        public byte n;
+        public byte nn;
+        public ushort nnn;
 
         public Instruction(ushort input)
         {
 
-            //TODO: transforma instructiuni in bucati tip, x, y, n, nn, nnn
+            //Totul este foarte neoptimizat pentru ca doar incerc sa fac ceva care functioneaza
+
+
+            //TODO: optimizeaza
             BitArray instructiune = new BitArray(16);
 
+            for(int i = 15; i >= 0; i--)
+            {
+                instructiune[i] = input % 2 > 0;
+                input /= 2;
+            }
 
-
+            for(int i = 0; i < 16; i++)
+            {
+                if(instructiune[i])
+                {
+                    Console.Write(1);
+                }
+                else
+                {
+                    Console.Write(0);
+                }
+            }
+            Console.WriteLine();
 
             byte pow = 1;
-            for(int i = 0; i < 8; i++)
+            for(int i = 3; i >= 0; i--)
             {
-                Console.WriteLine("instructiune[{0}] = {1}", i, instructiune[i]);
                 if (instructiune[i]) tip += pow;
                 pow *= 2;
             }
 
             pow = 1;
-            for (int i = 8; i < 16; i++)
+            for (int i = 7; i >= 4; i--)
             {
                 if (instructiune[i]) x += pow;
                 pow *= 2;
             }
 
             pow = 1;
-            for (int i = 16; i < 24; i++)
+            for (int i = 11; i >= 8; i--)
             {
                 if (instructiune[i]) y += pow;
                 pow *= 2;
             }
 
             pow = 1;
-            for (int i = 24; i < 32; i++)
+            for (int i = 15; i >= 12; i--)
             {
                 if (instructiune[i]) n += pow;
                 pow *= 2;
             }
 
+            pow = 1;
+            for (int i = 15; i >= 8; i--)
+            {
+                if (instructiune[i]) nn += pow;
+                pow *= 2;
+            }
 
-            Console.WriteLine("Avem: {0}, {1}, {2}, {3}", tip, x, y, n);
-            Console.ReadKey();
+            ushort shortpow = 1;
+            for (int i = 15; i >= 4; i--)
+            {
+                if (instructiune[i]) nnn += shortpow;
+                shortpow *= 2;
+            }
+            Console.WriteLine("Avem: Tip:{0}, X:{1}, Y:{2}, N:{3}, NN:{4}, NNN:{5}", tip, x, y, n, nn, nnn);
         }
 
     }
--- chip-8/Program.cs
+++ chip-8/Program.cs
@@ -14,41 +14,80 @@ namespace chip_8
         byte soundTimer;
         Stack<ushort> stack;
         bool[] display;
-        ushort[] memory;
+        ushort[] Memory;
         byte[] registers;
 
         static void Main(string[] args)
         {
-            Instruction test = new Instruction(12345);
+            Program p = new Program();
+            p.Emulator();
         }
 
+        private static ushort HexToUint(string input)
+        {
+            return Convert.ToUInt16(input, 16);
+        }
 
         private void Emulator()
         {
             registers = new byte[16];
-            memory = new ushort[4096];
+            Memory = new ushort[4096];
             display = new bool[2048];
             stack = new Stack<ushort>();
+            PC = 0;
+
+            //TODO: sa citim dintr-un fisier programul
+            Memory[0] = 224;
+            Memory[1] = 41514;
+            Memory[2] = 24588;
+            Memory[3] = 24840;
+            Memory[4] = 53279;
+            Memory[5] = 28681;
+            Memory[6] = 41529;
+            Memory[7] = 53279;
+            Memory[8] = 4096;
 
             while (true)
             {
-                Decode(Fetch());
-                //Execute();
+                Execute(Fetch());
+                Console.ReadKey();
             }
         }
 
         private Instruction Fetch()
         {
-            // Read instruction
-
+            //TODO: metoda asta de citire a instructiunilor este oribila
+            Instruction inst = new Instruction(Memory[PC / 2]);
             PC += 2;
-            return new Instruction(1234);
+            return inst;
         }
 
-        private void Decode(Instruction instruction)
+        private void Execute(Instruction instruction)
         {
-            switch (instruction)
+            switch(instruction.tip)
             {
+                case 0:
+                    if(instruction.nnn == 224)
+                    {
+                        // clear screen
+                        Console.WriteLine("Sterge ecran");
+                    }
+                    break;
+                case 1:
+                    PC = instruction.nnn;
+                    break;
+                case 6:
+                    registers[instruction.x] = instruction.nn;
+                    break;
+                case 7:
+                    registers[instruction.x] += instruction.nn;
+                    break;
+                case 10:
+                    I = instruction.nnn;
+                    break;
+                case 13:
+                    Console.WriteLine("deseneaza ceva");
+                    break;
                 default:
                     break;
             }
